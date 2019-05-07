0e5b60221175cd0209836b13a7a4529fea500592
YifanShenSZ <yshen57@jhu.edu>
2019-05-07 12:34:28 -0400

    5/7/2019

--- Main.f90
+++ Main.f90
@@ -595,10 +595,8 @@ subroutine GenerateNadVibSInput()
     character*2::chartemp
     integer::i,j,istate,jstate,iorder,NOrder
     integer,allocatable,dimension(:)::indice
-    real*8::L,RMSDenergy,RMSDdH,RMSDDegH,RMSDDegdH,L1,RMSDenergy1,RMSDdH1,RMSDDegH1,RMSDDegdH1
     real*8,dimension(InternalDimension)::qPrecursor,qSuccessor,freqPrecursor,freqSuccessor
     real*8,dimension(CartesianDimension)::rSuccesor
-    real*8,dimension(NHdExpansionCoefficients)::c
     real*8,dimension(InternalDimension,InternalDimension)::HPrecursor,HSuccessor
     real*8,dimension(InternalDimension,CartesianDimension)::BPrecursor,BSuccessor
     real*8,dimension(InternalDimension,InternalDimension,NState,NState)::Htemp
@@ -626,49 +624,44 @@ subroutine GenerateNadVibSInput()
     call WilsonGFMethod(freqSuccessor,HSuccessor,InternalDimension,BSuccessor,MoleculeDetail.mass,MoleculeDetail.NAtoms)
     if(minval(freqSuccessor)<-1d-14) write(*,*)'Warning: imaginary frequency found for successor'
     !Reformat Hd expansion coefficient into NadVibS format
-    call HdEC2c(Hd_HdEC,c,NHdExpansionCoefficients)
-    call L_RMSD(c,L,RMSDenergy,RMSDdH,RMSDDegH,RMSDDegdH)
-    call OriginShift(qSuccessor)!Shift origin to ground state minimum
-    call HdEC2c(Hd_HdEC,c,NHdExpansionCoefficients)
-    call L_RMSD(c,L1,RMSDenergy1,RMSDdH1,RMSDDegH1,RMSDDegdH1)
-    if(dAbs(L1-L)>1d-14.or.dAbs(RMSDenergy1-RMSDenergy)>1d-14.or.dAbs(RMSDdH1-RMSDdH)>1d-14.or.dAbs(RMSDDegH1-RMSDDegH)>1d-14.or.dAbs(RMSDDegdH1-RMSDDegdH)>1d-14) write(*,*)'Warning'
-    NOrder=0!Determine the highest order used
-    do i=1,NHdExpansionBasis
-        if(Hd_EBNR(i).order>NOrder) NOrder=Hd_EBNR(i).order
-    end do
-    allocate(indice(NOrder))
-    do jstate=1,NState!Allocate storage space of HdEC
-        do istate=jstate,NState
-            allocate(HdEC(istate,jstate).Order(0:NOrder))
-            do iorder=0,NOrder
-                allocate(HdEC(istate,jstate).Order(iorder).Array(iCombination(InternalDimension+iorder-1,iorder)))
-                HdEC(istate,jstate).Order(iorder).Array=0d0
+        call OriginShift(qSuccessor)!Shift origin to ground state minimum
+        NOrder=0!Determine the highest order used
+        do i=1,NHdExpansionBasis
+            if(Hd_EBNR(i).order>NOrder) NOrder=Hd_EBNR(i).order
+        end do
+        allocate(indice(NOrder))
+        do jstate=1,NState!Allocate storage space of HdEC
+            do istate=jstate,NState
+                allocate(HdEC(istate,jstate).Order(0:NOrder))
+                do iorder=0,NOrder
+                    allocate(HdEC(istate,jstate).Order(iorder).Array(iCombination(InternalDimension+iorder-1,iorder)))
+                    HdEC(istate,jstate).Order(iorder).Array=0d0
+                end do
             end do
         end do
-    end do
-    do iorder=0,NOrder
-        indice=1
-        i=WhichExpansionBasis(iorder,indice(1:iorder))
-        forall(istate=1:NState,jstate=1:NState,istate>=jstate)
-            HdEC(istate,jstate).Order(iorder).Array(1)=Hd_HdEC(istate,jstate).Array(i)
-        end forall
-        do j=2,size(HdEC(1,1).Order(iorder).Array)
-            indice(1)=indice(1)+1
-            do i=1,iorder
-                if(indice(i)>InternalDimension) then
-                    indice(i)=1
-                    indice(i+1)=indice(i+1)+1
-                end if
-            end do
-            do i=iorder-1,1,-1
-                if(indice(i)<indice(i+1)) indice(i)=indice(i+1)
-            end do
+        do iorder=0,NOrder
+            indice=1
             i=WhichExpansionBasis(iorder,indice(1:iorder))
             forall(istate=1:NState,jstate=1:NState,istate>=jstate)
-                HdEC(istate,jstate).Order(iorder).Array(j)=Hd_HdEC(istate,jstate).Array(i)
+                HdEC(istate,jstate).Order(iorder).Array(1)=Hd_HdEC(istate,jstate).Array(i)
             end forall
+            do j=2,size(HdEC(1,1).Order(iorder).Array)
+                indice(1)=indice(1)+1
+                do i=1,iorder
+                    if(indice(i)>InternalDimension) then
+                        indice(i)=1
+                        indice(i+1)=indice(i+1)+1
+                    end if
+                end do
+                do i=iorder-1,1,-1
+                    if(indice(i)<indice(i+1)) indice(i)=indice(i+1)
+                end do
+                i=WhichExpansionBasis(iorder,indice(1:iorder))
+                forall(istate=1:NState,jstate=1:NState,istate>=jstate)
+                    HdEC(istate,jstate).Order(iorder).Array(j)=Hd_HdEC(istate,jstate).Array(i)
+                end forall
+            end do
         end do
-    end do
     !Definition of dshift and Tshift see Schuurman & Yarkony 2008 JCP 128 eq. (12)
     dshift=matmul(transpose(HPrecursor),qSuccessor-qPrecursor)
     Tshift=matmul(transpose(HPrecursor),HSuccessor)
